ae431af783b55a63c50455d0e6b3e4826ecc1ef5
Jane <jane@MacBook-Air-Jane.local>
2023-02-22 23:06:30 +0200

    Created form for calculating wall tiles and added js script

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+function getSquareOfTheWall(height1, width1, height2, width2, height3, width3, height4, width4, doorH, doorW, windowH, windowW) {
+
+    return (height1 * width1) + (height2 * width2) + (height3 * width3) + (height4 * width4) - (doorH * doorW) - (windowH * windowW);
+
+}
+function renderResult() {
+    let height1 = document.getElementById('h1').value;
+    let width1 = document.getElementById('w1').value;
+    let height2 = document.getElementById('h2').value;
+    let width2 = document.getElementById('w2').value;
+    let height3 = document.getElementById('h3').value;
+    let width3 = document.getElementById('w3').value;
+    let height4 = document.getElementById('h4').value;
+    let width4 = document.getElementById('w4').value;
+    let doorH = document.getElementById('door-h').value;
+    let doorW = document.getElementById('door-w').value;
+    let windowH = document.getElementById('win-h').value;
+    let windowW = document.getElementById('win-w').value;
+    document.getElementById('res1').innerHTML = getSquareOfTheWall(
+        height1, width1, height2, width2, height3, width3, height4, width4, doorH, doorW, windowH, windowW
+    )
+}
+
